a7c3a72639cd9dbeb1e984e36deee7cd24d314f3
Alexandr <oleksandrpatsora@gmail.com>
2019-07-30 16:25:26 +0300

    rehubs

---
+++ Ui rehub/Assets/Prefabs/UI/Rooms/RoomsCanvases.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsCanvases : MonoBehaviour
+{
+    [SerializeField]
+    private CreateOrJoinRoomCanvase _createOrJoinRoomCanvase;
+
+    public CreateOrJoinRoomCanvase CreateOrJoinRoomCanvase { get { return _createOrJoinRoomCanvase; } }
+
+    [SerializeField]
+    private CurrentRoomCanvas _currentRoomCanvas;
+
+    public CurrentRoomCanvas CurrentRoomCanvas { get { return _currentRoomCanvas; } }
+
+    private void Awake()
+    {
+        FirstInitialize();
+    }
+
+    private void FirstInitialize()
+    {
+        CreateOrJoinRoomCanvase.FirstInitialize(this);
+        CurrentRoomCanvas.FirstInitialize(this);
+    }
+}
---
+++ Ui rehub/Assets/Scripts/TestConnect.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        Debug.Log("Connecting to server.", this);
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override  void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server", this);
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName, this);
+        if (!PhotonNetwork.InLobby)
+            PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected from server from reason" + cause.ToString(), this);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("Joined Lobby");
+    }
+
+}
